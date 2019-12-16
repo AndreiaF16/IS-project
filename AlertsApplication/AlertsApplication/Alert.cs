4bc2c160062705a5cc95f3a0549a73c18d1e97b8
David Simões <2170978@my.ipleiria.pt>
2019-12-16 15:19:45 +0000

    adding alerts

---
+++ AlertsApplication/AlertsApplication/Alert.cs
@@ -0,0 +1,13 @@
+﻿namespace ProjectXML
+{
+    internal class Alert
+    {
+        public int ID { get; set; }
+        public string Type { get; set; }
+        public string Operator { get; set; }
+        public decimal Value { get; set; }
+        public decimal Value2 { get; set; }
+        public bool Enable { get; set; }
+        public string Description { get; set; }
+    }
+}
