bc81325ab1f645e2d47b7158a680f29aaeb45e32
David Simões <2170978@my.ipleiria.pt>
2019-12-16 11:55:41 +0000

    Alerts and Presistence applications

---
+++ AssociationWithDatabase/AssociationWithDatabase/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AssociationWithDatabase
+{
+    internal class Sensor
+    {
+        public int SensorID { get; set; }
+        public decimal Temperature { get; set; }
+        public decimal Humidity { get; set; }
+        public int Battery { get; set; }
+        public DateTime Timestamp { get; set; }
+        public bool ActiveAlert { get; set; }
+        public string AlertDescription { get; set; }
+    }
+}
